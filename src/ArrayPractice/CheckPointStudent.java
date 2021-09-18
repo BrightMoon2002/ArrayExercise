161a045e39ec06020d2935377002e65918d05268
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 01:02:22 +0700

    Add creat array and size student

--- src/ArrayPractice/CheckPointStudent.java
+++ src/ArrayPractice/CheckPointStudent.java
@@ -4,6 +4,9 @@ public class CheckPointStudent {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
         System.out.println("Creat array have point of student. if point > 5, student pass the Exam. Check How many Students pass the Exam");
+        int[] array;
+        int size;
+        
 
     }
 }
