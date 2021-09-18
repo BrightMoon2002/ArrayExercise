4b7f73f8e130a98e160a338b66148598626da046
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 00:24:47 +0700

    Add instructor

--- src/ArrayPractice/CheckMinInTheArray.java
+++ src/ArrayPractice/CheckMinInTheArray.java
@@ -3,6 +3,7 @@ package ArrayPractice;
 public class CheckMinInTheArray {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
+        System.out.println("Check the Min number in the Array. Creat the Array and check Min");
         
     }
 }
