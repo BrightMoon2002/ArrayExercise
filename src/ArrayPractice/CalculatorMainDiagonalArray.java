e7b7daaddabd59897c274fe04ddea5853f4c9644
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 01:54:54 +0700

    Add creat array with input size and input number

--- src/ArrayPractice/CalculatorMainDiagonalArray.java
+++ src/ArrayPractice/CalculatorMainDiagonalArray.java
@@ -1,9 +1,35 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class CalculatorMainDiagonalArray {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
-        System.out.println("Creat a dimensional array and calcultor the main diagonal of this array:");
+        System.out.println("Creat a dimensional array and calculator the main diagonal of this array:");
+        int[][] array;
+        int sizeRow;
+        int sizeColumn;
+
+        do {
+            Scanner inputSizeRow = new Scanner(System.in);
+            System.out.println("Enter the Array size Row:");
+            sizeRow = inputSizeRow.nextInt();
+            Scanner inputSizeColumn = new Scanner(System.in);
+            System.out.println("Enter the Array size Column:");
+            sizeColumn = inputSizeColumn.nextInt();
+            if (sizeRow <= 0 || sizeColumn <= 0) {
+                System.out.println("The size does not exceed");
+            }
+        } while (sizeRow <= 0 || sizeColumn <= 0);
+
+        array = new int[sizeRow][sizeColumn];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                Scanner inputNumber = new Scanner(System.in);
+                System.out.println("Enter the number in the index:" + i + ", " + j);
+                array[i][j] = inputNumber.nextInt();
+            }
+        }
         
     }
 }
