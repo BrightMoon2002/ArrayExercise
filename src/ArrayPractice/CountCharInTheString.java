b4e4c2b5b2d5a05574c40860477023f0b8d28686
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 02:22:20 +0700

    Add creat string and word

--- src/ArrayPractice/CountCharInTheString.java
+++ src/ArrayPractice/CountCharInTheString.java
@@ -1,5 +1,7 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class CountCharInTheString {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
@@ -7,6 +9,11 @@ public class CountCharInTheString {
         String string;
         int count = 0;
 
-        
+        Scanner inputString = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        string = inputString.nextLine();
+        Scanner inputWord = new Scanner(System.in);
+        System.out.println("Enter the char:");
+        String word = inputWord.nextLine();
     }
 }
