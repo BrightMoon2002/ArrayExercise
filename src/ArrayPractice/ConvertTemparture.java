29cdaed6fb422c4e877e0e54dc4bc65cbdddf639
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-18 16:56:10 +0700

    Add author convert temparature

---
+++ src/ArrayPractice/ConvertTemparture.java
@@ -0,0 +1,8 @@
+package ArrayPractice;
+
+public class ConvertTemparture {
+    public static void main(String[] args) {
+        System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
+        
+    }
+}
