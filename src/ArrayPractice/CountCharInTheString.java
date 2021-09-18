c6f2d9f16f7b63f231256dc2a7da1bb813591a71
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 02:52:29 +0700

    change string to char and check char

--- src/ArrayPractice/CountCharInTheString.java
+++ src/ArrayPractice/CountCharInTheString.java
@@ -7,13 +7,34 @@ public class CountCharInTheString {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
         System.out.println("generate a string and count how many times each character in the string occurs");
         String string;
-        int count = 0;
 
         Scanner inputString = new Scanner(System.in);
         System.out.println("Enter the String:");
         string = inputString.nextLine();
-        Scanner inputWord = new Scanner(System.in);
-        System.out.println("Enter the char:");
-        String word = inputWord.nextLine();
+        char word = 't';
+        try{
+            System.out.println("Moi ban nhap 1 ky tu");
+            word = (char) System.in.read();
+            System.out.println("Ky tu vua nhap:" + word); // đưa lên đây để khi read() gặp lỗi nó sẽ không in ký tự mặc định.
+        }catch(Exception e){
+            System.out.println("Nhap loi!");
+        }
+        //check length of string System.out.println(string.length());
+        int result = getCount(string, word);
+        if (result == 0) {
+            System.out.printf("%s is not in the String", word);
+        } else {
+            System.out.printf("%s appears %d in the String", word, result);
+        }
+    }
+
+    private static int getCount(String string,  char word) {
+        int count = 0;
+        for (int i = 0; i < string.length(); i++) {
+            if (word == string.charAt(i)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
