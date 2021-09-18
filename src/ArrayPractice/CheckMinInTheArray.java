cba798d0bf6e5b4ef586dba2547794fd97b3ad18
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 00:28:08 +0700

    Add size in the Array

--- src/ArrayPractice/CheckMinInTheArray.java
+++ src/ArrayPractice/CheckMinInTheArray.java
@@ -1,10 +1,24 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class CheckMinInTheArray {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
         System.out.println("Check the Min number in the Array. Creat the Array and check Min");
         int[] array;
         int size;
+
+        do {
+            Scanner inputSize = new Scanner(System.in);
+            System.out.println("Enter the size of Array");
+            size = inputSize.nextInt();
+            if (size <= 0) {
+                System.out.println("The Array size is greater than 0");
+            }
+        } while (size <= 0);
+
+        
     }
+}
 }
