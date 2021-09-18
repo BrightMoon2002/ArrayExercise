a8321202be9fbf01f8f4661e54733ff7aa2f4f34
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-18 16:59:52 +0700

    Add scanner in convert temparature

--- src/ArrayPractice/ConvertTemparture.java
+++ src/ArrayPractice/ConvertTemparture.java
@@ -1,8 +1,11 @@
 package ArrayPractice;
 
+import java.util.Scanner;
+
 public class ConvertTemparture {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
-        
+        System.out.println("Convert the Temparature from C to F or vice versa");
+        Scanner scanner = new Scanner(System.in);
     }
 }
