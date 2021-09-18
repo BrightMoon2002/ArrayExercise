d986db24447cc85e0f193c23b66e291631a4e34b
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 01:52:38 +0700

    Add calculator the main diagonal of the array

---
+++ src/ArrayPractice/CalculatorMainDiagonalArray.java
@@ -0,0 +1,9 @@
+package ArrayPractice;
+
+public class CalculatorMainDiagonalArray {
+    public static void main(String[] args) {
+        System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
+        System.out.println("Creat a dimensional array and calcultor the main diagonal of this array:");
+        
+    }
+}
