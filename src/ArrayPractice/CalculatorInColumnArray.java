bc22931d1b4cbc4af15bafcf836dc29fa497b90e
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 01:27:40 +0700

    Creat array and size

--- src/ArrayPractice/CalculatorInColumnArray.java
+++ src/ArrayPractice/CalculatorInColumnArray.java
@@ -3,5 +3,10 @@ package ArrayPractice;
 public class CalculatorInColumnArray {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
+        System.out.println("Creat a dimensional array and calculator in the column of this array");
+
+        int[] array;
+        int size;
+        
     }
 }
