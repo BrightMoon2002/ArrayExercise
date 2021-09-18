40c4483d7ee07366374c4ae38aca969f5e19843e
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 02:16:14 +0700

    Add file calculator char in the string

---
+++ src/ArrayPractice/CountCharInTheString.java
@@ -0,0 +1,12 @@
+package ArrayPractice;
+
+public class CountCharInTheString {
+    public static void main(String[] args) {
+        System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
+        System.out.println("generate a string and count how many times each character in the string occurs");
+        String string;
+        int count = 0;
+
+        
+    }
+}
