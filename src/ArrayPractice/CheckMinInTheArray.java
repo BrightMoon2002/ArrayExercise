e2e48438571ca9189c3d79114b4e09a34e4a523e
Hoang Duy Nguyen <kiemda02@gmail.com>
2021-09-19 00:25:33 +0700

    Add creat array and size

--- src/ArrayPractice/CheckMinInTheArray.java
+++ src/ArrayPractice/CheckMinInTheArray.java
@@ -4,6 +4,7 @@ public class CheckMinInTheArray {
     public static void main(String[] args) {
         System.out.println("Author is Nguyễn Hoàng Duy - C0821H1");
         System.out.println("Check the Min number in the Array. Creat the Array and check Min");
-        
+        int[] array;
+        int size;
     }
 }
